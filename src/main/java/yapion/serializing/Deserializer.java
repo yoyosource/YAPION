21abf61eb2aaeb5f66b3aef2acfaac4613797166
YoyoSource <yoyosource@nidido.de>
2020-08-07 15:19:30 +0200

    Update README

--- src/main/java/yapion/serializing/Deserializer.java
+++ src/main/java/yapion/serializing/Deserializer.java
@@ -5,6 +5,9 @@ import yapion.hierarchy.YAPIONAny;
 public interface Deserializer<T> {
 
     String type();
+    default String primitiveType() {
+        return "";
+    }
     T deserialize(YAPIONAny yapionAny);
 
 }
