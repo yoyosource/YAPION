c09a8097d76490c700fe5108ca4c916722299979
YoyoSource <yoyosource@nidido.de>
2020-08-14 10:46:59 +0200

    Add YAPIONPacket

---
+++ src/main/java/yapion/packet/YAPIONPacket.java
@@ -0,0 +1,76 @@
+package yapion.packet;
+
+import yapion.annotations.YAPIONLoad;
+import yapion.annotations.YAPIONLoadExclude;
+import yapion.annotations.YAPIONSave;
+import yapion.annotations.YAPIONSaveExclude;
+import yapion.hierarchy.types.YAPIONObject;
+import yapion.serializing.YAPIONSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@YAPIONSave(context = "*")
+@YAPIONLoad(context = "*")
+public class YAPIONPacket {
+
+    @YAPIONSaveExclude(context = "*")
+    @YAPIONLoadExclude(context = "*")
+    private long lastModified = 0;
+    @YAPIONSaveExclude(context = "*")
+    @YAPIONLoadExclude(context = "*")
+    private long lastCreated = 0;
+    @YAPIONSaveExclude(context = "*")
+    @YAPIONLoadExclude(context = "*")
+    private YAPIONObject cache = null;
+
+    private final String type;
+    private final Map<String, Object> payload = new HashMap<>();
+
+    public YAPIONPacket(String type) {
+        this.type = type;
+    }
+
+    public YAPIONPacket add(String key, Object value) {
+        lastModified = System.currentTimeMillis();
+        payload.put(key, value);
+        return this;
+    }
+
+    public Object get(String key) {
+        return payload.get(key);
+    }
+
+    public long length() {
+        return getYAPION().toString().length();
+    }
+
+    public YAPIONObject getYAPION() {
+        if (cache != null && lastModified == lastCreated) {
+            return cache;
+        }
+        lastCreated = lastModified;
+        cache = YAPIONSerializer.serialize(this);
+        return cache;
+    }
+
+    public YAPIONObject toObject() {
+        return getYAPION();
+    }
+
+    public YAPIONObject getObject() {
+        return getYAPION();
+    }
+
+    @Override
+    public String toString() {
+        return "YAPIONPacket{" +
+                "lastModified=" + lastModified +
+                ", lastCreated=" + lastCreated +
+                ", cache=" + cache +
+                ", type='" + type + '\'' +
+                ", payload=" + payload +
+                '}';
+    }
+
+}
