84e554b7b7df9c9bd6de0c5b1780013b3413fca3
YoyoSource <yoyosource@nidido.de>
2020-10-07 19:43:30 +0200

    Add JavaDoc

--- src/main/java/yapion/utils/IdentifierUtils.java
+++ src/main/java/yapion/utils/IdentifierUtils.java
@@ -1,14 +1,29 @@
 package yapion.utils;
 
+import yapion.hierarchy.types.YAPIONObject;
+import yapion.hierarchy.types.YAPIONVariable;
+import yapion.serializing.YAPIONDeserializer;
+import yapion.serializing.YAPIONSerializer;
+import yapion.serializing.serializer.other.EnumSerializer;
+
 public class IdentifierUtils {
 
     /**
-     *
+     * This variable is used in the {@link YAPIONSerializer}
+     * and {@link YAPIONDeserializer} for identifying an
+     * {@link YAPIONObject} type by its {@link Class#getTypeName()}.
+     * In the serialization this variable is used as the
+     * key of a {@link YAPIONVariable} and will be called
+     * type variable.
      */
     public static final String TYPE_IDENTIFIER = "@type";
 
     /**
-     *
+     * This variable is used in the {@link EnumSerializer}
+     * for identifying an {@link Enum} by its {@link Class#getTypeName()}.
+     * In the serialization this variable is used as the
+     * key of a {@link YAPIONVariable} and will be calles
+     * enum variable.
      */
     public static final String ENUM_IDENTIFIER = "@enum";
 
