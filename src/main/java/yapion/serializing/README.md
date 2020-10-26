ceeb86f1b1916654b45843ffdfcbab28916df523
YoyoSource <yoyosource@nidido.de>
2020-10-26 12:31:14 +0100

    Add PackageRemapping to TypeReMapper

--- src/main/java/yapion/serializing/README.md
+++ src/main/java/yapion/serializing/README.md
@@ -1,7 +1,6 @@
 # Serializing
 
-There are some design choices, that I want to address in this Text.
-As there are so many I will group them by Type.
+There are some specific design choices, that a special and maybe unintuitive. As this is the case I want to address all of them for the serializer and deserializer. As there are many special design choices I will group them by Type.
 
 ## Enum
 Enums are quite tricky to handle with. This is why I need to handle them specially throughout serializing and deserializing.
@@ -9,23 +8,23 @@ As every `enum` extends `java.lang.Enum` I need to abstract enum serialization f
 a special `InternalSerializer` just handling the enum serialization and deserialization. As I cannot say if a given TypeName is an
 `enum` or not, I needed a way to identify a `YAPIONObject` as one.
 
-The typical enum structure:
+Because of this the typical enum structure is:
 ```
 {@type(java.lang.Enum)@enum(...)value(...)ordinal(...)}
 ```
 
-The YAPIONObject variable *@†ype* just defines the serializer to use. As for the actual type the enum should have there is the `@enum` variable.
-This variable is the `Class.getTypeName()` value. The `value` variable will be set to the `Enum.name()` and the `ordinal` variable to `Enum.ordinal()`.
-For deserialization the ordinal gets checked for quick deserialization. The name of this retrieved enum will be checked against the `value` variable and if
+The YAPIONObject variable `@†ype` just defines the serializer to use. As for the actual type the enum should have, there is the `@enum` variable.
+This variable is the `Class.getTypeName()` value of the `enum` to serialize. The `value` variable will be set to the `Enum.name()` and the `ordinal` variable
+to `Enum.ordinal()`. For deserialization the ordinal gets checked for quick deserialization. The name of this retrieved enum will be checked against the `value` variable and if
 not equal I will loop through all enum values, retrieved by `Class.getEnumConstants()`.
 
 In the `ContextManager` the enum will be handled specially to save enums by default. Because of this you do not need to explicitly put any save annotation at this field.
 To exclude an enum field in serialization you need to explicitly exclude it with the annotation.
-
-## ContextManager
 
 ## InternalSerializer
 
 ## Public API
 
 ## Not Serializable Stuff
+
+## TypeReMapper
--- src/main/java/yapion/serializing/TypeReMapper.java
+++ src/main/java/yapion/serializing/TypeReMapper.java
@@ -1,6 +1,8 @@
 package yapion.serializing;
 
 import yapion.exceptions.serializing.YAPIONClassTypeException;
+import yapion.serializing.serializer.other.YAPIONSerializerPacket;
+import yapion.utils.YAPIONTreeIterator;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +15,20 @@ public class TypeReMapper {
         if (typeMappings.containsKey(type)) {
             return typeMappings.get(type);
         }
-        return type;
+        String copiedType = type;
+        while (!typeMappings.containsKey(type + ".")) {
+            if (type.isEmpty()) {
+                return copiedType;
+            }
+            if (!type.contains(".")) {
+                return copiedType;
+            }
+            type = type.substring(0, type.lastIndexOf('.'));
+        }
+        if (!type.isEmpty() && typeMappings.containsKey(type + ".")) {
+            return typeMappings.get(type + ".") + copiedType.substring(type.length() + 1);
+        }
+        return copiedType;
     }
 
     private void checkClassTypeValidity(String type) {
@@ -28,26 +43,71 @@ public class TypeReMapper {
         }
     }
 
-    private void put(String fromType, String toType) {
+    private void putClass(String fromType, String toType) {
         checkClassTypeValidity(fromType);
         checkClassTypeValidity(toType);
         typeMappings.put(fromType, toType);
     }
 
-    public void addClassMapping(String fromType, String toType) {
-        put(fromType, toType);
+    public TypeReMapper addClassMapping(String fromType, String toType) {
+        putClass(fromType, toType);
+        return this;
     }
 
-    public void addClassMapping(Class<?> fromType, String toType) {
-        put(fromType.getTypeName(), toType);
+    public TypeReMapper addClassMapping(Class<?> fromType, String toType) {
+        putClass(fromType.getTypeName(), toType);
+        return this;
     }
 
-    public void addClassMapping(String fromType, Class<?> toType) {
-        put(fromType, toType.getTypeName());
+    public TypeReMapper addClassMapping(String fromType, Class<?> toType) {
+        putClass(fromType, toType.getTypeName());
+        return this;
     }
 
-    public void addClassMapping(Class<?> fromType, Class<?> toType) {
-        put(fromType.getTypeName(), toType.getTypeName());
+    public TypeReMapper addClassMapping(Class<?> fromType, Class<?> toType) {
+        putClass(fromType.getTypeName(), toType.getTypeName());
+        return this;
+    }
+
+    private void checkPackageTypeValidity(String type) {
+        if (!type.endsWith(".")) {
+            throw new YAPIONClassTypeException("No valid package");
+        }
+        if (type.contains("$")) {
+            throw new YAPIONClassTypeException("No valid package");
+        }
+    }
+
+    private void putPackage(String fromType, String toType) {
+        checkPackageTypeValidity(fromType);
+        checkPackageTypeValidity(toType);
+        typeMappings.put(fromType, toType);
+    }
+
+    public TypeReMapper addPackageMapping(String fromType, String toType) {
+        putPackage(fromType, toType);
+        return this;
+    }
+
+    public TypeReMapper addPackageMapping(Package fromType, String toType) {
+        putPackage(fromType.getName() + ".", toType);
+        return this;
+    }
+
+    public TypeReMapper addPackageMapping(String fromType, Package toType) {
+        putPackage(fromType, toType.getName() + ".");
+        return this;
+    }
+
+    public TypeReMapper addPackageMapping(Package fromType, Package toType) {
+        putPackage(fromType.getName() + ".", toType.getName() + ".");
+        return this;
+    }
+
+    public TypeReMapper copy() {
+        TypeReMapper typeReMapper = new TypeReMapper();
+        typeMappings.forEach((s, s2) -> typeReMapper.typeMappings.put(s, s2));
+        return typeReMapper;
     }
 
 }
