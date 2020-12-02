172c44e89889a4d5bbe649dac47e66c23c87cb01
YoyoSource <yoyosource@nidido.de>
2020-12-02 22:36:18 +0100

    Add AtomicIntegerArraySerializer Add AtomicLongArraySerializer

---
+++ src/main/java/yapion/serializing/serializer/object/atomic/AtomicIntegerArraySerializer.java
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+// YAPION
+// Copyright (C) 2019,2020 yoyosource
+
+package yapion.serializing.serializer.object.atomic;
+
+import yapion.hierarchy.typegroups.YAPIONAnyType;
+import yapion.hierarchy.types.YAPIONArray;
+import yapion.hierarchy.types.YAPIONObject;
+import yapion.serializing.InternalSerializer;
+import yapion.serializing.data.DeserializeData;
+import yapion.serializing.data.SerializeData;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+import static yapion.utils.IdentifierUtils.TYPE_IDENTIFIER;
+
+public class AtomicIntegerArraySerializer implements InternalSerializer<AtomicIntegerArray> {
+
+    @Override
+    public String type() {
+        return "java.util.concurrent.atomic.AtomicIntegerArray";
+    }
+
+    @Override
+    public YAPIONAnyType serialize(SerializeData<AtomicIntegerArray> serializeData) {
+        YAPIONObject yapionObject = new YAPIONObject();
+        yapionObject.add(TYPE_IDENTIFIER, type());
+        yapionObject.add("length", serializeData.object.length());
+
+        int[] ints = new int[serializeData.object.length()];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = serializeData.object.get(i);
+        }
+        yapionObject.add("values", serializeData.serialize(ints));
+        return yapionObject;
+    }
+
+    @Override
+    public AtomicIntegerArray deserialize(DeserializeData<? extends YAPIONAnyType> deserializeData) {
+        YAPIONObject yapionObject = (YAPIONObject) deserializeData.object;
+        int length = yapionObject.getValue("length", 0).get();
+        YAPIONArray yapionArray = yapionObject.getArray("values");
+        int[] ints = new int[length];
+        for (int i = 0; i < length; i++) {
+            ints[i] = (Integer) deserializeData.deserialize(yapionArray.get(i));
+        }
+        return new AtomicIntegerArray(ints);
+    }
+}
---
+++ src/main/java/yapion/serializing/serializer/object/atomic/AtomicLongArraySerializer.java
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+// YAPION
+// Copyright (C) 2019,2020 yoyosource
+
+package yapion.serializing.serializer.object.atomic;
+
+import yapion.hierarchy.typegroups.YAPIONAnyType;
+import yapion.hierarchy.types.YAPIONArray;
+import yapion.hierarchy.types.YAPIONObject;
+import yapion.serializing.InternalSerializer;
+import yapion.serializing.data.DeserializeData;
+import yapion.serializing.data.SerializeData;
+
+import java.util.concurrent.atomic.AtomicLongArray;
+
+import static yapion.utils.IdentifierUtils.TYPE_IDENTIFIER;
+
+public class AtomicLongArraySerializer implements InternalSerializer<AtomicLongArray> {
+
+    @Override
+    public String type() {
+        return "java.util.concurrent.atomic.AtomicLongArray";
+    }
+
+    @Override
+    public YAPIONAnyType serialize(SerializeData<AtomicLongArray> serializeData) {
+        YAPIONObject yapionObject = new YAPIONObject();
+        yapionObject.add(TYPE_IDENTIFIER, type());
+        yapionObject.add("length", serializeData.object.length());
+
+        long[] longs = new long[serializeData.object.length()];
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = serializeData.object.get(i);
+        }
+        yapionObject.add("values", serializeData.serialize(longs));
+        return yapionObject;
+    }
+
+    @Override
+    public AtomicLongArray deserialize(DeserializeData<? extends YAPIONAnyType> deserializeData) {
+        YAPIONObject yapionObject = (YAPIONObject) deserializeData.object;
+        int length = yapionObject.getValue("length", 0).get();
+        YAPIONArray yapionArray = yapionObject.getArray("values");
+        long[] longs = new long[length];
+        for (int i = 0; i < length; i++) {
+            longs[i] = (Long) deserializeData.deserialize(yapionArray.get(i));
+        }
+        return new AtomicLongArray(longs);
+    }
+
+}
