b1f72c970ce90177a39ed952f83ac206e23a9022
YoyoSource <yoyosource@nidido.de>
2020-12-02 22:02:28 +0100

    Add Copyright Notice

--- src/main/java/yapion/serializing/serializer/object/security/PublicKeySerializer.java
+++ src/main/java/yapion/serializing/serializer/object/security/PublicKeySerializer.java
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// YAPION
+// Copyright (C) 2019,2020 yoyosource
+
 package yapion.serializing.serializer.object.security;
 
 import yapion.annotations.deserialize.YAPIONLoadExclude;
