552e11ceb05d543db78093c46ad0c61e884f6d1e
yoyosource <yoyosource@nidido.de>
2021-03-22 08:38:44 +0100

    Add MapRetrieveTemplate.template Add CreateInterface

--- src/test/java/yapion/interfaceCreator/CreateInterface.java
+++ src/test/java/yapion/interfaceCreator/CreateInterface.java
@@ -13,56 +13,107 @@
 
 package yapion.interfaceCreator;
 
+import lombok.NonNull;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CreateInterface {
 
-    private static List<String> stringList = new ArrayList<>();
+    private static List<String> typeList = new ArrayList<>();
+    private static int count = 0;
 
     public static void main(String[] args) {
-        stringList.add("@NonNull String");
-        stringList.add("char");
-        stringList.add("boolean");
-        stringList.add("byte");
-        stringList.add("short");
-        stringList.add("int");
-        stringList.add("long");
-        stringList.add("@NonNull BigInteger");
-        stringList.add("float");
-        stringList.add("double");
-        stringList.add("@NonNull BigDecimal");
+        typeList.add("@NonNull String");
+        typeList.add("char");
+        typeList.add("boolean");
+        typeList.add("byte");
+        typeList.add("short");
+        typeList.add("int");
+        typeList.add("long");
+        typeList.add("@NonNull BigInteger");
+        typeList.add("float");
+        typeList.add("double");
+        typeList.add("@NonNull BigDecimal");
 
-        fillTemplate("default boolean hasValue($ key) {\nreturn hasValue(new YAPIONValue<>(key));\n}");
-        fillTemplate("default boolean hasValue($ key, YAPIONType yapionType) {\nreturn hasValue(new YAPIONValue<>(key), yapionType);\n}");
-        fillTemplate("default <T> boolean hasValue($ key, Class<T> type) {\nreturn hasValue(new YAPIONValue<>(key), type);\n}");
-        fillTemplate("default YAPIONAnyType getYAPIONAnyType($ key) {\nreturn getYAPIONAnyType(new YAPIONValue<>(key));\n}");
-        fillTemplate("default YAPIONObject getObject($ key) {\nreturn getObject(new YAPIONValue<>(key));\n}");
-        fillTemplate("default void getObject($ key, Consumer<YAPIONObject> valueConsumer, Runnable noValue) {\ngetObject(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
-        fillTemplate("default YAPIONArray getArray($ key) {\nreturn getArray(new YAPIONValue<>(key));\n}");
-        fillTemplate("default void getArray($ key, Consumer<YAPIONArray> valueConsumer, Runnable noValue) {\ngetArray(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
-        fillTemplate("default YAPIONMap getMap($ key) {\nreturn getMap(new YAPIONValue<>(key));\n}");
-        fillTemplate("default void getMap($ key, Consumer<YAPIONMap> valueConsumer, Runnable noValue) {\ngetMap(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
-        fillTemplate("default YAPIONPointer getPointer($ key) {\nreturn getPointer(new YAPIONValue<>(key));\n}");
-        fillTemplate("default void getPointer($ key, Consumer<YAPIONPointer> valueConsumer, Runnable noValue) {\ngetPointer(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
-        fillTemplate("default YAPIONValue getValue($ key) {\nreturn getValue(new YAPIONValue<>(key));\n}");
-        fillTemplate("default void getValue($ key, Consumer<YAPIONValue> valueConsumer, Runnable noValue) {\ngetValue(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
-        fillTemplate("default <T> YAPIONValue<T> getValue($ key, Class<T> type) {\nreturn getValue(new YAPIONValue<>(key), type);\n}");
-        fillTemplate("default <T> YAPIONValue<T> getValueOrDefault($ key, Class<T> type, T defaultValue) {\nreturn getValueOrDefault(new YAPIONValue<>(key), type, defaultValue);\n}");
-        fillTemplate("default <T> void getValue($ key, Class<T> type, Consumer<YAPIONValue<T>> valueConsumer, Runnable noValue) {\ngetValue(new YAPIONValue<>(key), type, valueConsumer, noValue);\n}");
-        fillTemplate("default <T> YAPIONValue<T> getValue($ key, T type) {\nreturn getValue(new YAPIONValue<>(key), type);\n}");
-        fillTemplate("default <T> YAPIONValue<T> getValueOrDefault($ key, T defaultValue) {\nreturn getValueOrDefault(new YAPIONValue<>(key), defaultValue);\n}");
-        fillTemplate("default <T> T getPlainValue($ key) {\nreturn getPlainValue(new YAPIONValue<>(key));\n}");
-        fillTemplate("default <T> T getPlainValueOrDefault($ key, T defaultValue) {\nreturn getPlainValueOrDefault(new YAPIONValue<>(key), defaultValue);\n}");
-        fillTemplate("default <T> void getPlainValue($ key, Consumer<T> valueConsumer, Runnable noValue) {\ngetPlainValue(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default boolean hasValue($0 key) {\nreturn hasValue(new YAPIONValue<>(key));\n}");
+        evalTemplate("default boolean hasValue($0 key, YAPIONType yapionType) {\nreturn hasValue(new YAPIONValue<>(key), yapionType);\n}");
+        evalTemplate("default <T> boolean hasValue($0 key, Class<T> type) {\nreturn hasValue(new YAPIONValue<>(key), type);\n}");
+        evalTemplate("default YAPIONAnyType getYAPIONAnyType($0 key) {\nreturn getYAPIONAnyType(new YAPIONValue<>(key));\n}");
+        evalTemplate("default YAPIONObject getObject($0 key) {\nreturn getObject(new YAPIONValue<>(key));\n}");
+        evalTemplate("default void getObject($0 key, Consumer<YAPIONObject> valueConsumer, Runnable noValue) {\ngetObject(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default YAPIONArray getArray($0 key) {\nreturn getArray(new YAPIONValue<>(key));\n}");
+        evalTemplate("default void getArray($0 key, Consumer<YAPIONArray> valueConsumer, Runnable noValue) {\ngetArray(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default YAPIONMap getMap($0 key) {\nreturn getMap(new YAPIONValue<>(key));\n}");
+        evalTemplate("default void getMap($0 key, Consumer<YAPIONMap> valueConsumer, Runnable noValue) {\ngetMap(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default YAPIONPointer getPointer($0 key) {\nreturn getPointer(new YAPIONValue<>(key));\n}");
+        evalTemplate("default void getPointer($0 key, Consumer<YAPIONPointer> valueConsumer, Runnable noValue) {\ngetPointer(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default YAPIONValue getValue($0 key) {\nreturn getValue(new YAPIONValue<>(key));\n}");
+        evalTemplate("default void getValue($0 key, Consumer<YAPIONValue> valueConsumer, Runnable noValue) {\ngetValue(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        evalTemplate("default <T> YAPIONValue<T> getValue($0 key, Class<T> type) {\nreturn getValue(new YAPIONValue<>(key), type);\n}");
+        evalTemplate("default <T> YAPIONValue<T> getValueOrDefault($0 key, Class<T> type, T defaultValue) {\nreturn getValueOrDefault(new YAPIONValue<>(key), type, defaultValue);\n}");
+        evalTemplate("default <T> void getValue($0 key, Class<T> type, Consumer<YAPIONValue<T>> valueConsumer, Runnable noValue) {\ngetValue(new YAPIONValue<>(key), type, valueConsumer, noValue);\n}");
+        evalTemplate("default <T> YAPIONValue<T> getValue($0 key, T type) {\nreturn getValue(new YAPIONValue<>(key), type);\n}");
+        evalTemplate("default <T> YAPIONValue<T> getValueOrDefault($0 key, T defaultValue) {\nreturn getValueOrDefault(new YAPIONValue<>(key), defaultValue);\n}");
+        evalTemplate("default <T> T getPlainValue($0 key) {\nreturn getPlainValue(new YAPIONValue<>(key));\n}");
+        evalTemplate("default <T> T getPlainValueOrDefault($0 key, T defaultValue) {\nreturn getPlainValueOrDefault(new YAPIONValue<>(key), defaultValue);\n}");
+        evalTemplate("default <T> void getPlainValue($0 key, Consumer<T> valueConsumer, Runnable noValue) {\ngetPlainValue(new YAPIONValue<>(key), valueConsumer, noValue);\n}");
+        System.out.println();
+        System.out.println(count);
     }
 
-    private static void fillTemplate(String template) {
-        if (!template.contains("$")) return;
-        for (String s : stringList) {
-            System.out.println();
-            System.out.println(template.replace("$", s));
+    private static class Template {
+
+        private String template;
+        private int depth = -1;
+        private List<List<String>> templateReplaceList = new ArrayList<>();
+        private List<String> results = new ArrayList<>();
+
+        private Template(String template) {
+            this.template = template;
+            while (true) {
+                if (!template.contains("$" + (depth + 1))) break;
+                depth++;
+            }
         }
+
+        public void add(@NonNull List<String> templateReplacements) {
+            templateReplaceList.add(templateReplacements.stream().filter(Objects::nonNull).collect(Collectors.toList()));
+        }
+
+        public void replace() {
+            if (templateReplaceList.size() != depth + 1) return;
+            results.clear();
+            replace(template, 0);
+        }
+
+        private void replace(String current, int index) {
+            if (index > depth) {
+                results.add(current);
+                return;
+            }
+            for (String s : templateReplaceList.get(index)) {
+                replace(current.replace("$" + index, s), index + 1);
+            }
+        }
+
+        public List<String> getResults() {
+            return new ArrayList<>(results);
+        }
+
+    }
+
+    private static void evalTemplate(String templateString) {
+        Template template = new Template(templateString);
+        template.add(typeList);
+        template.replace();
+        template.getResults().forEach(s -> {
+            System.out.println();
+            System.out.println(s);
+            count++;
+        });
     }
 
 }
