0c4e57a47ed85a6f0c109b523fc131843ea562d3
yoyosource <yoyosource@nidido.de>
2021-05-31 12:31:29 +0200

    Fix YAPIONValue

--- src/main/java/yapion/hierarchy/types/YAPIONValue.java
+++ src/main/java/yapion/hierarchy/types/YAPIONValue.java
@@ -116,7 +116,7 @@ public class YAPIONValue<T> extends YAPIONValueType {
 
     @Override
     protected long referenceValueProvider(ReferenceFunction referenceFunction) {
-        return getType().getReferenceValue() ^ valueHandler.referenceValue(referenceFunction);
+        return getType().getReferenceValue() ^ valueHandler.referenceValue(referenceFunction) & 0x7FFFFFFFFFFFFFFFL;
     }
 
     void toStrippedYAPION(AbstractOutput abstractOutput) {
