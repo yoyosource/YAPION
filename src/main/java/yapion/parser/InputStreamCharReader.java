47c44e5cbc186891b3d740dfec5e386efd4e41e2
yoyosource <yoyosource@nidido.de>
2021-07-03 11:51:19 +0200

    Optimize InputStreamCharReader

--- src/main/java/yapion/parser/InputStreamCharReader.java
+++ src/main/java/yapion/parser/InputStreamCharReader.java
@@ -65,65 +65,45 @@ public class InputStreamCharReader implements CharReader {
             }
             if (i >>> 7 == 0) {
                 available--;
-                return (char) i;
-            }
-            if (i >>> 5 == 0b110) {
-                int j = inputStream.read();
-                if (j == -1 && !stopOnStreamEnd) {
-                    throw new YAPIONParser.ParserSkipException();
-                }
-                if (j >>> 6 == 0b10) {
-                    i &= 0b00011111;
-                    j &= 0b00111111;
-                    available -= 2;
-                    return (char) (i << 6 | j);
-                }
+                if (i >= 0x0000 && i <= 0x007F) return (char) i;
+            } else if (i >>> 5 == 0b110) {
+                int j = readNext();
+                i &= 0b00011111;
+                available -= 2;
+                int result = i << 6 | j;
+                if (result >= 0x0080 && result <= 0x07FF) return (char) result;
             } else if (i >>> 4 == 0b1110) {
-                int j = inputStream.read();
-                if (j == -1 && !stopOnStreamEnd) {
-                    throw new YAPIONParser.ParserSkipException();
-                }
-                if (j >>> 6 == 0b10) {
-                    int k = inputStream.read();
-                    if (k == -1 && !stopOnStreamEnd) {
-                        throw new YAPIONParser.ParserSkipException();
-                    }
-                    if (k >>> 6 == 0b10) {
-                        i &= 0b00001111;
-                        j &= 0b00111111;
-                        k &= 0b00111111;
-                        available -= 3;
-                        return (char) (i << 12 | j << 6 | k);
-                    }
-                }
+                int j = readNext();
+                int k = readNext();
+                i &= 0b00001111;
+                available -= 3;
+                int result = i << 12 | j << 6 | k;
+                if (result >= 0x0800 && result <= 0x0FFF) return (char) result;
+                if (result >= 0x1000 && result <= 0xFFFF) return (char) result;
             } else if (i >>> 3 == 0b11110) {
-                int j = inputStream.read();
-                if (j == -1 && !stopOnStreamEnd) {
-                    throw new YAPIONParser.ParserSkipException();
-                }
-                if (j >>> 6 == 0b10) {
-                    int k = inputStream.read();
-                    if (k == -1 && !stopOnStreamEnd) {
-                        throw new YAPIONParser.ParserSkipException();
-                    }
-                    if (k >>> 6 == 0b10) {
-                        int l = inputStream.read();
-                        if (l == -1 && !stopOnStreamEnd) {
-                            throw new YAPIONParser.ParserSkipException();
-                        }
-                        if (l >>> 6 == 0b10) {
-                            i &= 0b00000111;
-                            j &= 0b00111111;
-                            k &= 0b00111111;
-                            l &= 0b00111111;
-                            available -= 4;
-                            return (char) (i << 18 | j << 12 | k << 6 | l);
-                        }
-                    }
-                }
+                int j = readNext();
+                int k = readNext();
+                int l = readNext();
+                i &= 0b00000111;
+                available -= 4;
+                int result = i << 18 | j << 12 | k << 6 | l;
+                if (result >= 0x10000 && result <= 0x3FFFF) return (char) result;
+                if (result >= 0x40000 && result <= 0xFFFFF) return (char) result;
+                if (result >= 0x100000 && result <= 0x10FFFF) return (char) result;
             }
             throw new YAPIONIOException("Unrecognized UTF-8 Sequence");
         };
+    }
+
+    private int readNext() throws IOException {
+        int current = inputStream.read();
+        if (current == -1 && !stopOnStreamEnd) {
+            throw new YAPIONParser.ParserSkipException();
+        }
+        if (current >>> 6 == 0b10) {
+            return current & 0b00111111;
+        }
+        throw new YAPIONIOException("Unrecognized UTF-8 Sequence");
     }
 
     @Override
