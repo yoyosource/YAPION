8c71780a0bbf50dc8318a2b37fe29053e83dd332
yoyosource <yoyosource@nidido.de>
2021-08-18 18:29:40 +0200

    Fix YAPIONDeserializerTest.testPrimitive on Windows

--- src/test/java/yapion/serializing/YAPIONDeserializerTest.java
+++ src/test/java/yapion/serializing/YAPIONDeserializerTest.java
@@ -54,7 +54,7 @@ public class YAPIONDeserializerTest {
 
     @Test
     public void testPrimitive() {
-        Object object = YAPIONDeserializer.deserialize(YAPIONParser.parse("{@type(yapion.serializing.YAPIONTestObjects$TestPrimitive)t(false)T(true)b(0B)B(0B)s(0S)S(0S)i(0)I(0)l(0L)L(0L)d(0.0)D(0.0)f(0.0F)F(0.0F)bi(0BI)bd(0BD)c(' ')C(' ')string()stringB{@type(java.lang.StringBuilder)string()}stringb{@type(java.lang.StringBuffer)string()}file{@type(java.io.File)path(" + getUserHome() + File.separator + "YAPI)}}"));
+        Object object = YAPIONDeserializer.deserialize(YAPIONParser.parse("{@type(yapion.serializing.YAPIONTestObjects$TestPrimitive)t(false)T(true)b(0B)B(0B)s(0S)S(0S)i(0)I(0)l(0L)L(0L)d(0.0)D(0.0)f(0.0F)F(0.0F)bi(0BI)bd(0BD)c(' ')C(' ')string()stringB{@type(java.lang.StringBuilder)string()}stringb{@type(java.lang.StringBuffer)string()}file{@type(java.io.File)path(" + getUserHome().replaceAll("\\\\([nrt])", "\\\\$1") + File.separator + "YAPI)}}"));
         TestPrimitive toEqual = new TestPrimitive();
         assertThat(object, is(toEqual));
         TestPrimitive primitive = (TestPrimitive) object;
