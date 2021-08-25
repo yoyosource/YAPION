ccb3a91855a17f0189c767f27e4487ba0fb1be3d
yoyosource <yoyosource@nidido.de>
2021-08-25 14:47:49 +0200

    Add ParseOptions Add StreamOptions Add FileOptions

--- src/main/java/yapion/parser/InputStreamCharsets.java
+++ src/main/java/yapion/parser/InputStreamCharsets.java
@@ -14,14 +14,45 @@
 package yapion.parser;
 
 public enum InputStreamCharsets {
-
+    /**
+     * Just the default 0x00 - 0x7F
+     */
     US_ASCII,
 
     /**
      * Also known as ISO_8859_1
+     * Just the default 0x00 - 0xFF
      */
     LATIN_1,
 
-    UTF_8;
-
+    /**
+     * <table>
+     *   <tr>
+     *     <th>#</th>
+     *     <th>Bits</th>
+     *     <th>BitPattern</th>
+     *   </tr>
+     *   <tr>
+     *       <td>1</td>
+     *       <td>7</td>
+     *       <td>0xxxxxxx</td>
+     *   </tr>
+     *   <tr>
+     *       <td>2</td>
+     *       <td>11</td>
+     *       <td>110xxxxx 10xxxxxx</td>
+     *   </tr>
+     *   <tr>
+     *       <td>3</td>
+     *       <td>16</td>
+     *       <td>1110xxxx 10xxxxxx 10xxxxxx</td>
+     *   </tr>
+     *   <tr>
+     *       <td>4</td>
+     *       <td>21</td>
+     *       <td>11110xxx 10xxxxxx 10xxxxxx 10xxxxxx</td>
+     *   </tr>
+     * </table>
+     */
+    UTF_8
 }
