73f1d129fd904facb6eae5b490c5f8aca3e654fb
yoyosource <yoyosource@nidido.de>
2022-07-05 17:12:33 +0200

    Add PairSerializer.kt Add TripleSerializer.kt Add ResultSerializer.kt

---
+++ YAPION-Databind-Kotlin/src/main/kotlin/yapion/serializing/serializer/object/ResultSerializer.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019,2020,2021 yoyosource
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package yapion.serializing.serializer.`object`
+
+import yapion.annotations.api.SerializerImplementation
+import yapion.hierarchy.api.groups.YAPIONAnyType
+import yapion.hierarchy.types.YAPIONObject
+import yapion.serializing.data.DeserializeData
+import yapion.serializing.data.SerializeData
+import yapion.serializing.serializer.FinalInternalSerializer
+
+@SerializerImplementation(since = "1.0.0")
+class ResultSerializer : FinalInternalSerializer<Result<Any>> {
+
+    override fun type(): Class<*> {
+        return Result::class.java
+    }
+
+    override fun serialize(serializeData: SerializeData<Result<Any>>?): YAPIONAnyType {
+        return YAPIONObject().add("success", serializeData!!.`object`.isSuccess)
+            .add("value", serializeData.serialize(serializeData.`object`.getOrNull()))
+            .add("exception", serializeData.serialize(serializeData.`object`.exceptionOrNull()))
+    }
+
+    override fun deserialize(deserializeData: DeserializeData<out YAPIONAnyType>?): Result<Any> {
+        val yapionObject = deserializeData!!.`object` as YAPIONObject
+        return if (yapionObject.getBoolean("success")) {
+            Result.success(deserializeData.deserialize(yapionObject.get("value")))
+        } else {
+            Result.failure(deserializeData.deserialize(yapionObject.get("exception")))
+        }
+    }
+}
---
+++ YAPION-Databind-Kotlin/src/main/kotlin/yapion/serializing/serializer/object/tuples/TripleSerializer.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019,2020,2021 yoyosource
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package yapion.serializing.serializer.`object`.tuples
+
+import yapion.annotations.api.SerializerImplementation
+import yapion.hierarchy.api.groups.YAPIONAnyType
+import yapion.hierarchy.types.YAPIONObject
+import yapion.serializing.data.DeserializeData
+import yapion.serializing.data.SerializeData
+import yapion.serializing.serializer.FinalInternalSerializer
+
+@SerializerImplementation(since = "1.0.0")
+class TripleSerializer: FinalInternalSerializer<Triple<Any, Any, Any>> {
+
+    override fun type(): Class<*> {
+        return Triple::class.java
+    }
+
+    override fun serialize(serializeData: SerializeData<Triple<Any, Any, Any>>?): YAPIONAnyType {
+        return YAPIONObject().add("first", serializeData!!.serialize(serializeData.`object`.first))
+            .add("second", serializeData.serialize(serializeData.`object`.second))
+            .add("third", serializeData.serialize(serializeData.`object`.third))
+    }
+
+    override fun deserialize(deserializeData: DeserializeData<out YAPIONAnyType>?): Triple<Any, Any, Any> {
+        val yapionObject = deserializeData!!.`object` as YAPIONObject
+        return Triple(
+            deserializeData.deserialize(yapionObject.get("first")),
+            deserializeData.deserialize(yapionObject.get("second")),
+            deserializeData.deserialize(yapionObject.get("third"))
+        )
+    }
+}
